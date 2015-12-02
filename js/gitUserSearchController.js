2829cc43dc284a107ba1acf560513d2d18d58856
jamierose <jrose111284@gmail.com>
2015-12-02 12:19:27 +0000

    Github API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,19 +1,10 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+  var searchResource = $resource('https://api.github.com/search/users');
   var self = this;
+
   self.doSearch = function (){
-    self.searchResult = {
-      items: [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-     ]
-   };
+    self.searchResult = searchResource.get(
+    { q: self.searchTerm }
+    );
   };
 }]);
